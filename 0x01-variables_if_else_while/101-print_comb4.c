ed6cb40fb7b8f3f41917a89534dc02d767801b4c
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2022-12-12 04:29:51 +0100

    fixing 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 89.
+ * main - Prints numbers between 012 to 789.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, e;
+	int i, e, g;
 
 	i = 48;
 	e = 48;
+	g = 48;
 
 	while (e < 58)
 	{
 		i = 48;
 		while (i < 58)
 		{
-			if (e != i && e < i)
+			g = 48;
+			while (g < 58)
 			{
-				putchar(e);
-				putchar(i);
-				if (i == 57 && e == 56)
+				if (e != i && e != g && i != g && e < i && i < g)
 				{
-					break;
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
 				}
-				putchar(',');
-				putchar(' ');
+				g++;
 			}
 			i++;
 		}
