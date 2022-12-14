579f3c706c233c2bd47afe8e670dbbaaad6aa80a
Solocorpboss <s.austine16@gmail.com>
2022-12-13 18:36:17 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,25 @@
-#include <unistd.h>
-#include "alx.h"
+#include "main.h"
 
 /**
-*_putchar . nrites the character c to stdout
-* @c: the character to print 
-*
-*on error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-  return(write(1, &c, 1));
-}
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
