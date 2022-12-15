23572cdcd924ef153a162ba121cdcb9df89a2b5d
Solocorpboss <s.austine16@gmail.com>
2022-12-14 19:30:40 -0800

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+n - causes an infinite loop
+* Return: 0
+* */
+
+int main(void)
+{
+	        int i;
+
+		        printf("Infinite loop incoming :(\n");
+
+			        i = 0;
+
+				       /* while (i < 10)*/
+						       /*{*/
+								                /*putchar(i);*/
+										        /*}*/
+
+					        printf("Infinite loop avoided! \\o/\n");
+
+						        return (0);
+}
+i
