02aab65fb93fb5eb9f7a20e3faa3500a71f14f03
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2022-12-16 00:10:00 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints number
+ * *Write a function that prints the numbers, from 0 to 9, followed by a new line
+ *  Prototype: void print_most_numbers(void);
+ * Do not print 2 and 4
+ * You can only use _putchar twice in your code
  */
 
 void print_most_numbers(void)
