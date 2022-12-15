2a9cf579772b1d10684d88d796e17a742dbbb37d
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2022-12-16 00:13:15 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints more numbers
+ * more_numbers - Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
  * Return: 0 if successful
  */
 
