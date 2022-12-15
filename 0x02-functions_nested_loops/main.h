fcdda11b4acbe4aa46d882e9098ea2554f0f3811
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2022-12-15 03:37:42 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h> 
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -16,4 +18,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-#endif
+#endif /* MAIN_H */
