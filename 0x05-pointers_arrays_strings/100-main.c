8787eda29e0e91ab23a969bf539820721d0a2431
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2023-01-01 10:35:14 +0100

    Update 100-main.c

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -11,21 +11,21 @@ int main(void)
 {
   int nb;
 
-  nb = _atoi("98");
+  nb = atoi("98");
   printf("%d\n", nb);
-  nb = _atoi("-402");
+  nb = atoi("-402");
   printf("%d\n", nb);
-  nb = _atoi("          ------++++++-----+++++--98");
+  nb = atoi("          ------++++++-----+++++--98");
   printf("%d\n", nb);
-  nb = _atoi("214748364");
+  nb = atoi("214748364");
   printf("%d\n", nb);
-  nb = _atoi("0");
+  nb = atoi("0");
   printf("%d\n", nb);
-  nb = _atoi("Suite 402");
+  nb = atoi("Suite 402");
   printf("%d\n", nb);
-  nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+  nb = atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
   printf("%d\n", nb);
-  nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+  nb = atoi("---++++ -++ Sui - te -   402 #cisfun :)");
   printf("%d\n", nb);
   return (0);
 }
