1f2efd7d5fde53017a99b8a4718ba6fd50da3dc2
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2023-01-04 13:21:39 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,14 @@
  *@s: pointer block of memory to fill
  *Return: void
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
-	_putchar (*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
