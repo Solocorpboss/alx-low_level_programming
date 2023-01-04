797b2cf8f969f21b3e2194514992921f8c0490d8
Solocorpboss <117789863+Solocorpboss@users.noreply.github.com>
2023-01-04 14:23:56 +0100

    Task 1

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,17 +1,12 @@
 #include "main.h"
-/**
- *_puts_recursion - prints a string
- *@s: pointer block of memory to fill
- *Return: void
- */
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
 
-	_putchar (*s);
-	_puts_recursion(s + 1);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
 }
